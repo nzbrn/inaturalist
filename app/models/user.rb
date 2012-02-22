3fb53ce14c3acbbec5269111d9bf4d786641a21e
Patrick Davey <Patrick@egressive.com>
2012-02-22 14:31:40 +1300

    adding the home region field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   JEDI_MASTER_ROLE = 'admin'
 
   USER_GENDER = %w(Male Female)
+
+  HOME_REGIONS = ["Auckland","Bay of Plenty","Canterbury", "Gisbourne", "Hawkes Bay","Manawatu-Wanganui","Marlborough",
+    "Nelson City", "Northland", "Otago", "Southland", "Taranaki", "Tasman", "Waikato", "Wellington", "Westland", "Neslon-Marlborough", "No data"]
   
   devise :database_authenticatable, :registerable, :suspendable,
          :recoverable, :rememberable, :confirmable, :validatable, 
@@ -140,7 +143,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation, :icon, :description, :time_zone, :icon_url, :gender, :year_of_birth, :first_name, :last_name, :address, :phone_numbers_attributes, :expertise
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :icon, :description, :time_zone, :icon_url, :gender, :year_of_birth, :first_name, :last_name, :address, :phone_numbers_attributes, :expertise, :home_region
 
   serialize :expertise, Hash
 
