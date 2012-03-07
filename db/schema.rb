72c78e1769d89e01e196f065d61b84f99bf5ea8c
Patrick Davey <Patrick@egressive.com>
2012-03-07 15:42:29 +1300

    adding number_observed to observations

--- db/schema.rb
+++ db/schema.rb
@@ -420,6 +420,7 @@ ActiveRecord::Schema.define(:version => 20121128022641) do
     t.string   "positioning_device"
     t.boolean  "out_of_range"
     t.string   "license"
+    t.integer  "number_individuals"
   end
 
   add_index "observations", ["geom"], :name => "index_observations_on_geom", :spatial => true
