3accf8e8ddc051eec9faa7a228c7f92406e01861
Patrick Davey <Patrick@egressive.com>
2012-04-12 16:47:39 +1200

    adding the delayed job tasks.  Seems to start

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,72 +64,94 @@ after "deploy:update_code" do
   #symlink_sphinx
   #sphinx_configure
 end
-  desc "Create a symlink to a copy of config.yml that is outside the repos."
-  task :symlink_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/config.yml #{latest_release}/config/config.yml"
-    run "ln -s #{inat_config_shared_path}/settings.yml #{latest_release}/config/settings.yml"
+desc "Create a symlink to a copy of config.yml that is outside the repos."
+task :symlink_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/config.yml #{latest_release}/config/config.yml"
+  run "ln -s #{inat_config_shared_path}/settings.yml #{latest_release}/config/settings.yml"
+end
+
+desc "Create a symlink to a copy of database.yml that is outside the repos."
+task :symlink_db_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/database.yml #{latest_release}/config/database.yml"
+end
+
+desc "Create a symlink to domain, dev.domain or test.domain gmap_api_key.yml file"
+task :symlink_gmap_api_key, :hosts => "#{domain}" do
+  stage = fetch(:stage, 'production')
+  case stage
+  when 'test'
+    run "ln -s #{inat_config_shared_path}/test_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
+  when 'dev', 'development'
+    run "ln -s #{inat_config_shared_path}/dev_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
+  else
+    run "ln -s #{inat_config_shared_path}/production_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
+  end
+end
+
+desc "Create a symlink to a copy of smtp.yml that is outside the repos."
+task :symlink_smtp_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/smtp.yml #{latest_release}/config/smtp.yml"
+end
+
+desc "Create a symlink to a copy of sphinx.yml that is outside the repos."
+task :symlink_sphinx_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/sphinx.yml #{latest_release}/config/sphinx.yml"
+end
+
+desc "Create a symlink to a copy of s3.yml that is outside the repos."
+task :symlink_s3_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/s3.yml #{latest_release}/config/s3.yml"
+end
+
+# temp
+desc "Create a symlink to a copy of newrelic.yml that is outside the repos."
+task :symlink_newrelic_config, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/newrelic.yml #{latest_release}/config/newrelic.yml"
+end
+
+desc "Symlink to the common attachments dir"
+task :symlink_attachments, :hosts => "#{domain}" do
+  run "ln -s #{shared_path}/system/attachments #{latest_release}/public/attachments"
+end
+
+desc "Symlink to the common cache dir"
+task :symlink_cache, :hosts => "#{domain}" do
+  run "ln -s #{shared_path}/system/cache #{latest_release}/cache"
+  run "ln -s #{shared_path}/system/page_cache/observations #{latest_release}/public/observations"
+  run "ln -s #{shared_path}/system/page_cache/taxa #{latest_release}/public/taxa"
+  run "ln -s #{shared_path}/system/page_cache/places #{latest_release}/public/places"
+  run "ln -s #{shared_path}/system/page_cache/lists #{latest_release}/public/lists"
+end
+
+desc "Symlink the path to tilelite"
+task :symlink_observation_tiles, :hosts => "#{domain}" do
+  run "ln -s #{inat_config_shared_path}/tilelite/public #{shared_path}/system/page_cache/observations/tiles"
+end
+
+desc "Clear the cache directories"
+task :clear_cache, :hosts => "#{domain}" do
+  run "rm -rf #{shared_path}/system/cache/*"
+  run "rm -rf #{shared_path}/system/page_cache/observations/*"
+end
+
+# DelayedJob
+namespace :delayed_job do
+  desc "Start delayed_job process" 
+  task :start, :roles => :app do
+    run "cd #{current_path}; script/delayed_job start production" 
   end
 
-  desc "Create a symlink to a copy of database.yml that is outside the repos."
-  task :symlink_db_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/database.yml #{latest_release}/config/database.yml"
+  desc "Stop delayed_job process" 
+  task :stop, :roles => :app do
+    run "cd #{current_path}; script/delayed_job stop production" 
   end
-  
-  desc "Create a symlink to domain, dev.domain or test.domain gmap_api_key.yml file"
-  task :symlink_gmap_api_key, :hosts => "#{domain}" do
-    stage = fetch(:stage, 'production')
-    case stage
-    when 'test'
-      run "ln -s #{inat_config_shared_path}/test_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
-    when 'dev', 'development'
-      run "ln -s #{inat_config_shared_path}/dev_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
-    else
-      run "ln -s #{inat_config_shared_path}/production_gmaps_api_key.yml #{latest_release}/config/gmaps_api_key.yml"
-    end
+
+  desc "Restart delayed_job process" 
+  task :restart, :roles => :app do
+    run "cd #{current_path}; script/delayed_job restart production" 
   end
-  
-  desc "Create a symlink to a copy of smtp.yml that is outside the repos."
-  task :symlink_smtp_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/smtp.yml #{latest_release}/config/smtp.yml"
-  end
-  
-  desc "Create a symlink to a copy of sphinx.yml that is outside the repos."
-  task :symlink_sphinx_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/sphinx.yml #{latest_release}/config/sphinx.yml"
-  end
-  
-  desc "Create a symlink to a copy of s3.yml that is outside the repos."
-  task :symlink_s3_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/s3.yml #{latest_release}/config/s3.yml"
-  end
-  
-  # temp
-  desc "Create a symlink to a copy of newrelic.yml that is outside the repos."
-  task :symlink_newrelic_config, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/newrelic.yml #{latest_release}/config/newrelic.yml"
-  end
-  
-  desc "Symlink to the common attachments dir"
-  task :symlink_attachments, :hosts => "#{domain}" do
-    run "ln -s #{shared_path}/system/attachments #{latest_release}/public/attachments"
-  end
-  
-  desc "Symlink to the common cache dir"
-  task :symlink_cache, :hosts => "#{domain}" do
-    run "ln -s #{shared_path}/system/cache #{latest_release}/cache"
-    run "ln -s #{shared_path}/system/page_cache/observations #{latest_release}/public/observations"
-    run "ln -s #{shared_path}/system/page_cache/taxa #{latest_release}/public/taxa"
-    run "ln -s #{shared_path}/system/page_cache/places #{latest_release}/public/places"
-    run "ln -s #{shared_path}/system/page_cache/lists #{latest_release}/public/lists"
-  end
-  
-  desc "Symlink the path to tilelite"
-  task :symlink_observation_tiles, :hosts => "#{domain}" do
-    run "ln -s #{inat_config_shared_path}/tilelite/public #{shared_path}/system/page_cache/observations/tiles"
-  end
-  
-  desc "Clear the cache directories"
-  task :clear_cache, :hosts => "#{domain}" do
-    run "rm -rf #{shared_path}/system/cache/*"
-    run "rm -rf #{shared_path}/system/page_cache/observations/*"
-  end
+end
+
+after "deploy:start", "delayed_job:start" 
+after "deploy:stop", "delayed_job:stop" 
+after "deploy:restart", "delayed_job:restart"
