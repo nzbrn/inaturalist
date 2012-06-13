006baf23913b9d2c7f59e6b4d2a6591aaa2e391d
Kale Worsley <kale@egressive.com>
2012-06-13 14:15:00 +1200

    Tweak caching for staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,17 +6,17 @@ config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+config.action_controller.perform_caching             = false
 # config.action_controller.cache_store                 = :file_store, "#{RAILS_ROOT}/cache"
-config.cache_store                                   = :mem_cache_store, INAT_CONFIG["memcached"]
-config.action_view.cache_template_loading            = true
+# config.cache_store                                   = :mem_cache_store, INAT_CONFIG["memcached"]
+config.action_view.cache_template_loading            = false
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors if you bad email addresses should just be ignored
 config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => 'inat.nzbrn.org.nz' }
+config.action_mailer.default_url_options = { :host => 'staging.inat.egressive.com' }
 config.action_mailer.delivery_method = :sendmail
 
 # Set the logger to roll over monthly
